f9199e448f0c22ed4efd384ea82bac28d5e19277
neemiasbsilva <neemiasbsilva@gmail.com>
2022-08-16 12:28:43 -0300

    Beginners: [1008] - Salary

---
+++ beginner/salary_1008/Salary.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args)throws IOException{
+        try(Scanner scan = new Scanner(System.in)){
+            int number = scan.nextInt();
+            int hours = scan.nextInt();
+            float amount = scan.nextFloat();
+            float salary = amount*hours;
+            System.out.format("NUMBER = %d%n", number);
+            System.out.format("SALARY = U$ %.2f%n", salary);
+        }
+    }
+}
